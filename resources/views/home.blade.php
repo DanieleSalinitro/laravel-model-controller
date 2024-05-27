069274d3609bd0d0a9e8473fc1673b527bea95fb
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-05-27 18:05:28 +0200

    stampa cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,21 @@
 
 @section('content')
 <main>
-    <h1>Home page</h1>
+    <div class="container mt-5">
+        <div class="row">
+            @foreach($movies as $movie)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <p class="card-text">{{ $movie->description }}</p>
+                            <p class="card-text"><small class="text-muted">Data uscita: {{ $movie->date }}</small></p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </main>
 
 @endsection
